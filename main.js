0977b936ef48c9d40ffee6bbd85b44b9c8888268
Stoyann <stoyann.openclassrooms@gmail.com>
2021-09-17 14:40:46 +1100

    page index style

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const open_btn = document.querySelector(".open-btn");
-const close_btn = document.querySelector(".close-btn");
-const nav = document.querySelectorAll(".nav");
+import { navbar } from "./scripts/nav.js";
 
-open_btn.addEventListener("click", () => {
-  nav.forEach((nav_el) => nav_el.classList.add("visible"));
-});
-
-close_btn.addEventListener("click", () => {
-  nav.forEach((nav_el) => nav_el.classList.remove("visible"));
-});
+navbar();
