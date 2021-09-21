1de0098e7b058784036b4163889a2e1ea7778649
Stoyann <stoyann.openclassrooms@gmail.com>
2021-09-22 00:52:58 +1100

    design index

--- main.js
+++ main.js
@@ -8,16 +8,47 @@ burger.addEventListener("click", () => {
 });
 
 var root = document.documentElement;
-setTimeout(function () {
-  var percent = 55;
-  var DOMStyle = getComputedStyle(root);
-  root.style.setProperty("--nb-percent1", percent);
-  root.style.setProperty("--nb-percent-str1", '"' + percent + '%"');
-}, 1000);
+var percent = 22;
+timeVac(72.89);
+timeVac2(14.96);
+timeVac3(11.96);
+time2(34.62);
+time(55.16);
+function time(percentValue) {
+  setTimeout(function () {
+    var DOMStyle = getComputedStyle(root);
+    root.style.setProperty("--nb-percent", percentValue);
+    root.style.setProperty("--nb-percent-str", '"' + percentValue + '%"');
+  }, 1000);
+}
+function time2(percentValue) {
+  setTimeout(function () {
+    var DOMStyle = getComputedStyle(root);
+    root.style.setProperty("--nb-percent2", percentValue);
+    root.style.setProperty("--nb-percent-str2", '"' + percentValue + '%"');
+  }, 1000);
+}
 
-setTimeout(function () {
-  var percent2 = 34;
-  var DOMStyle = getComputedStyle(root);
-  root.style.setProperty("--nb-percent2", percent2);
-  root.style.setProperty("--nb-percent-str2", '"' + percent2 + '%"');
-}, 1000);
+function timeVac(percentValue) {
+  setTimeout(function () {
+    var DOMStyle = getComputedStyle(root);
+    root.style.setProperty("--nb-percent3", percentValue);
+    root.style.setProperty("--nb-percent-str3", '"' + percentValue + '%"');
+  }, 1000);
+}
+
+function timeVac2(percentValue) {
+  setTimeout(function () {
+    var DOMStyle = getComputedStyle(root);
+    root.style.setProperty("--nb-percent4", percentValue);
+    root.style.setProperty("--nb-percent-str4", '"' + percentValue + '%"');
+  }, 1000);
+}
+
+function timeVac3(percentValue) {
+  setTimeout(function () {
+    var DOMStyle = getComputedStyle(root);
+    root.style.setProperty("--nb-percent4", percentValue);
+    root.style.setProperty("--nb-percent-str5", '"' + percentValue + '%"');
+  }, 1000);
+}
