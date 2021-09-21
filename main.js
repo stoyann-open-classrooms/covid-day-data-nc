eef0cde56a3716194245469247046965600392ac
Stoyann <stoyann.openclassrooms@gmail.com>
2021-09-21 14:06:55 +1100

    today chiffres

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ burger.addEventListener("click", () => {
   let naviguation = document.querySelector(".naviguation");
   naviguation.classList.toggle("active");
 });
+
+var root = document.documentElement;
+var percent = 55.02;
+var percent2 = 34.34;
+setTimeout(function () {
+  var DOMStyle = getComputedStyle(root);
+  root.style.setProperty("--nb-percent1", percent);
+  root.style.setProperty("--nb-percent-str1", '"' + percent + '%"');
+}, 1000);
+
+setTimeout(function () {
+  var DOMStyle = getComputedStyle(root);
+  root.style.setProperty("--nb-percent2", percent2);
+  root.style.setProperty("--nb-percent-str2", '"' + percent2 + '%"');
+}, 1000);
